bbca6adfc88493fc6dd5644b1aab628bece32818
lidengbo <1094950646@qq.com>
2016-11-29 13:39:52 +0800

    创建项目

---
+++ Adam.HomeNews/HomeNews.Api/Index/INews.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeNews.Api.Index
+{
+   public interface INews
+    {
+       string NewId { get; set; }
+       DateTime NewCreateTime { get; set; }
+       string NewTitle { get; set; }
+       string NewContent { get; set; }
+    }
+}
